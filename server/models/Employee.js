462b35f66eeace15bf1ae1e1b035d379c6276010
notoveryet-51 <sray387.official@gmail.com>
2026-05-18 00:28:09 +0530

    Add Employee model with schema for name, email, and password

---
+++ server/models/Employee.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const EmployeeSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    }
+});
+
+export const EmployeeModel = mongoose.model('employee', EmployeeSchema);
