70ef1fc953ab0de8b7711b24bb6be6ceb3d59800
notoveryet-51 <sray387.official@gmail.com>
2026-05-18 00:27:34 +0530

    Add Home component with dashboard and welcome message

---
+++ client/src/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <p>Login verified! Welcome to the Home page.</p>
+        </div>
+    );
+}
+
+export default Home;
