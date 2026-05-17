348c5509c3da873ed802d267ff81f21166a7c907
notoveryet-51 <sray387.official@gmail.com>
2026-05-18 00:27:26 +0530

    Refactor App component to implement routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './Signup';
+import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
