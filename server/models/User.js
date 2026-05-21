288f9d537a044a9661b301e6e9e4ba714e985bb3
notoveryet-51 <sray387.official@gmail.com>
2026-05-21 14:49:14 +0530

    Enhance User model for OAuth support by adding googleId and githubId fields, and remove password requirement for OAuth users. Update package.json and package-lock.json to include passport and OAuth dependencies.

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,24 @@
 import mongoose from "mongoose";
+
 const UserSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: true,
     },
-
     email: {
       type: String,
       required: true,
       unique: true,
     },
-
     password: {
+      type: String, // Removed required: true so OAuth users can be created without a password
+    },
+    googleId: { // Added to track Google logins
       type: String,
-      required: true,
+    },
+    githubId: { // Added to track GitHub logins
+      type: String,
     },
   },
   {
