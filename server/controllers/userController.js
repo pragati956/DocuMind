7d1296a01d172b158bc134f95472c0db677c2dd7
Pragati Singh <pragatisingh933585@gmail.com>
2026-06-06 13:30:30 +0530

    Implemented secure password change functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const getProfile = async (req, res) => {
   try {
@@ -109,6 +110,73 @@ async (req, res) => {
       success: true,
       notifications:
         user.notificationPreferences,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+
+};
+export const changePassword =
+async (req, res) => {
+
+  try {
+
+    const {
+      currentPassword,
+      newPassword,
+    } = req.body;
+
+    const user =
+      await UserModel.findById(
+        req.user.id
+      );
+
+    if (!user) {
+
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+
+    }
+
+    const isMatch =
+      await bcrypt.compare(
+        currentPassword,
+        user.password
+      );
+
+    if (!isMatch) {
+
+      return res.status(400).json({
+        success: false,
+        message:
+          "Current password is incorrect",
+      });
+
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(
+        newPassword,
+        10
+      );
+
+    user.password =
+      hashedPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message:
+        "Password updated successfully",
     });
 
   } catch (error) {
